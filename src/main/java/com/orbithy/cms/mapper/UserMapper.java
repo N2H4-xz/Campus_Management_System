945264f053f280a9a71bd21c3bb52da9fd77a360
Hanserprpr <2041283903@qq.com>
2025-03-15 16:34:01 +0800

    UPDATE
    添加用户登录功能

--- src/main/java/com/orbithy/cms/mapper/UserMapper.java
+++ src/main/java/com/orbithy/cms/mapper/UserMapper.java
@@ -2,8 +2,16 @@ package com.orbithy.cms.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.orbithy.cms.data.vo.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
+
+    @Select("select id from user where student_id=#{SDUId}")
+    Integer getUserId(String SDUId);
+
+    @Insert("insert into user (username, password, student_id, major) values (#{username}, #{password}, #{SDUId}, 0)")
+    void addUser(String username, String password, String SDUId);
 }
