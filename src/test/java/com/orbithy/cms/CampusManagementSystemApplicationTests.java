e6ad4ec8372a83e951eb9d43f32f0fdbda3f77f7
Hanserprpr <2041283903@qq.com>
2025-03-15 10:51:54 +0800

    CREATE
    创建项目模板

---
+++ src/test/java/com/orbithy/cms/CampusManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbithy.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
