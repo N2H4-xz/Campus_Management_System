a05d8d24b293d40166d7c756977b8d25611adfed
STN2H4 <3256360589@qq.com>
2025-03-15 16:25:03 +0800

    改一下classes

--- src/main/java/com/orbithy/cms/data/vo/Classes.java
+++ src/main/java/com/orbithy/cms/data/vo/Classes.java
@@ -4,8 +4,9 @@ import com.baomidou.mybatisplus.annotation.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import com.baomidou.mybatisplus.core.enums.IEnum;
-
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -40,7 +41,7 @@ public class Classes {
     private Byte period;
 
     @TableField(value = "time")
-    private String time; // MySQL的SET类型存储为逗号分隔的字符串，如 "1,2,3"
+    private String time; // MySQL SET 类型存储为逗号分隔的字符串，如 "1,2,3"
 
     @TableField(value = "college")
     private String college;
@@ -53,6 +54,40 @@ public class Classes {
 
     @TableField(value = "type")
     private CourseType type;
+
+    @TableField(value = "capacity")
+    private Byte capacity;
+
+    // 处理 MySQL SET 类型，转换为 Java Set<Integer>
+    public Set<Integer> getTimeSet() {
+        if (this.time == null || this.time.isEmpty()) {
+            return Set.of();
+        }
+        return Set.of(this.time.split(","))
+                .stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+    }
+
+    public void setTimeSet(Set<Integer> timeSet) {
+        this.time = timeSet.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+    }
+
+    // 课程类型枚举
+    @Getter
+    public enum CourseType {
+        MANDATORY("必修"),
+        LIMITED("限选"),
+        ELECTIVE("任选");
+
+        @EnumValue  // 标记存储到数据库的字段
+        private final String value;
+
+        CourseType(String value) {
+            this.value = value;
+        }
+
+    }
 }
-
-
