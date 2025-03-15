3d0532ae8b69ac4d64c285ea7e646144faddbf30
STN2H4 <3256360589@qq.com>
2025-03-15 16:46:22 +0800

    Update Classes.java

--- src/main/java/com/orbithy/cms/data/vo/Classes.java
+++ src/main/java/com/orbithy/cms/data/vo/Classes.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class Classes {
     @TableId(type = IdType.AUTO)
     private Integer id;
-    
+
     private String name;
     private String category;
     private Byte point;
