6e4529a98a712e72772a594ce5d0dd8085540d8e
Hanserprpr <2041283903@qq.com>
2025-03-15 19:47:32 +0800

    UPDATE
    添加手动添加教师功能

--- src/main/java/com/orbithy/cms/controller/UserController.java
+++ src/main/java/com/orbithy/cms/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.orbithy.cms.controller;
 
 
+import com.orbithy.cms.data.vo.Result;
 import com.orbithy.cms.service.UserService;
 import jakarta.annotation.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Resource
     UserService userService;
+
+    /**
+     * 添加教师
+     *
+     * @param SDUId     教师工号
+     * @param password  密码
+     * @param username  用户名
+     * @param permission 权限
+     * @param secret    密钥
+     * @return ResponseEntity<Result>
+     */
+    @PostMapping("/addTeacher")
+    public ResponseEntity<Result> addTeacher(String SDUId, String password, String username, int permission, String secret) {
+        return userService.addTeacher(SDUId, password, username, permission, secret);
+    }
 }
--- src/main/java/com/orbithy/cms/service/UserService.java
+++ src/main/java/com/orbithy/cms/service/UserService.java
@@ -1,4 +1,35 @@
 package com.orbithy.cms.service;
 
+import com.orbithy.cms.data.vo.Result;
+import com.orbithy.cms.mapper.UserMapper;
+import com.orbithy.cms.utils.BcryptUtils;
+import com.orbithy.cms.utils.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private login login;
+
+    @Value("${spring.secret}")
+    private String secret1;
+
+    public ResponseEntity<Result> addTeacher(String SDUId, String password, String username, int permission, String secret) {
+        if (!Objects.equals(secret, secret1)) {
+            return ResponseUtil.build(Result.error(401, "Authentication failed"));
+        }
+        if (!login.isExisted(SDUId)) {
+            String passwd = BcryptUtils.encrypt(password);
+            userMapper.addTeacher(username, passwd, SDUId, permission);
+        }
+        return ResponseUtil.build(Result.ok());
+    }
 }
